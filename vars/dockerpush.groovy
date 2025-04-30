a5f4b10d596e2e6e14488991cd2331af88815952
sai_prasad_shetty <87171068+Saiprasad-1727@users.noreply.github.com>
2025-04-30 12:07:57 +0530

    Update dockerpush.groovy

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -7,3 +7,13 @@ def call(String credId, String imageName,String imageTag){
                     sh "docker push ${env.dockerHubUser}/${imageName}:${imageTag}"
           }
 }
+
+
+# withCredentials([usernamePassword(
+  #                  'credentialsId':"dockerHubCred",
+   #                 usernameVariable:"dockerHubUser/USERNAME"
+    #                passwordVariable:"dockerHubPASS/PASSWORD")]) {
+    #            sh "docker login -u ${env.dockerHubUser/USERNAME} -p ${env.dockerHubPass/PASSWORD} "
+    #            sh "docker image tag image:latest ${env.dockerHuvuser}/image:latest"
+    #            sh "docker push ${env.dockerHuvuser/USERNAME}/image:latest"
+    #            }
